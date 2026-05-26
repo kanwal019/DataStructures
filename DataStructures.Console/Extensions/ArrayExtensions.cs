8be4501ebd4a1fbcd7527c46a026bda3ec3a70d4
kanwal019 <kanwal019@gmail.com>
2026-05-27 00:33:59 +0530

    ✨Added extenstion to an array to display its items

---
+++ DataStructures.Console/Extensions/ArrayExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace DataStructures.Console.Extensions;
+
+public static class ArrayExtensions
+{
+    public static void Display<T>(this T[] array)
+    {
+        foreach (T item in array)
+        {
+            System.Console.Write($"{item} ");
+        }
+        System.Console.WriteLine();
+    }
+
+    public static void DisplayReverse<T>(this T[] array)
+    {
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            System.Console.Write($"{array[i]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
