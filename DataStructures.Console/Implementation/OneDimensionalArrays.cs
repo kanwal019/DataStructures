f5b46082c247d57006aeb7b6aec78f1d2e505876
kanwal019 <kanwal019@gmail.com>
2026-05-27 00:33:59 +0530

    ✨Added extenstion to an array to display its items

--- DataStructures.Console/Implementation/OneDimensionalArrays.cs
+++ DataStructures.Console/Implementation/OneDimensionalArrays.cs
@@ -1,3 +1,4 @@
+using DataStructures.Console.Extensions;
 using DataStructures.Core.Arrays;
 
 namespace DataStructures.Console.Implementation;
@@ -6,14 +7,18 @@ public class OneDimensionalArrays
 {
     public static void DisplayResult()
     {
-        Array<int> numbers = new(5);
+        int[] numbers = [10, 20, 30, 40, 50];
+        numbers.Display();
+        numbers.DisplayReverse();
 
-        numbers[0] = 10;
-        numbers[1] = 20;
-        numbers[2] = 30;
-        numbers[3] = 40;
-        numbers[4] = 50;
+        numbers[1] = 100;
+        numbers.Display();
+        numbers.DisplayReverse();
+    }
 
+    public static void DisplayResult2()
+    {
+        Array<int> numbers = new([10, 20, 30, 40, 50]);
         numbers.Display();
         numbers.DisplayReverse();
 
