546d6d805ca94c91c6671f1c3e6f127415e537b9
kanwal019 <kanwal019@gmail.com>
2026-06-10 23:27:19 +0530

    ♻️Implement LinearSearch class with DisplayResult method for searching array elements

---
+++ DataStructures.Console/Implementation/LinearSearchImplementation.cs
@@ -0,0 +1,28 @@
+using DataStructures.Console.Extensions;
+using DataStructures.Core.Searching;
+
+namespace DataStructures.Console.Implementation;
+
+public class LinearSearchImplementation
+{
+    public static void DisplayResult()
+    {
+        int[] numbers = new int[10];
+        for (int i = 0; i < 10; i++)
+        {
+            numbers[i] = i + 1;
+        }
+        numbers.Display();
+
+        int target = 5;
+        int index = LinearSearch.Search(numbers, target);
+        if (index != -1)
+        {
+            System.Console.WriteLine($"Element {target} found at index {index}.");
+        }
+        else
+        {
+            System.Console.WriteLine($"Element {target} not found in the list.");
+        }
+    }
+}
