351e3fcb49435d2a4ce2036b9c9291b06c87eaee
kanwal019 <kanwal019@gmail.com>
2026-06-16 23:55:15 +0530

    ✨Add Bubble Sort algorithm for generic array sorting

---
+++ DataStructures.Core/Sorting/BubbleSort.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace DataStructures.Core.Sorting;
+
+public static class BubbleSort
+{
+    public static void Sort<T>(T[] array) where T : IComparable<T>
+    {
+        if (array == null)
+        {
+            throw new ArgumentNullException(nameof(array), "Array cannot be null.");
+        }
+
+        int n = array.Length;
+        bool swapped;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (array[j].CompareTo(array[j + 1]) > 0)
+                {
+                    T temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped)
+            {
+                break;
+            }
+        }
+    }
+}
