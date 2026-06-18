447eb4a1064c7325227b09d22b7edafe9730cae8
kanwal019 <kanwal019@gmail.com>
2026-06-18 23:29:29 +0530

    ✨Add Insertion Sort algorithm for generic array sorting

---
+++ DataStructures.Core/Sorting/InsertionSort.cs
@@ -0,0 +1,27 @@
+namespace DataStructures.Core.Sorting;
+
+public static class InsertionSort
+{
+    public static void Sort<T>(T[] array) where T : IComparable<T>
+    {
+        if (array == null)
+        {
+            throw new ArgumentNullException(nameof(array), "Array cannot be null.");
+        }
+
+        int n = array.Length;
+
+        for (int i = 1; i < n; i++)
+        {
+            T key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j].CompareTo(key) > 0)
+            {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+        }
+    }
+}
