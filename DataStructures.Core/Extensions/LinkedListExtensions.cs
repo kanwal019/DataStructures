e399353ef55e8d8741d92be55b60f383161fbc2f
kanwal019 <kanwal019@gmail.com>
2026-06-22 23:44:49 +0530

    ✨Add MovingPointer class with methods for linked list manipulation and cycle detection

---
+++ DataStructures.Core/Extensions/LinkedListExtensions.cs
@@ -0,0 +1,26 @@
+using DataStructures.Core.LinkedLists.Basic;
+
+namespace DataStructures.Core.Extensions;
+
+public static class LinkedListExtensions
+{
+    public static bool HasCycle<T>(this MovingPointer<T> list)
+    {
+        var node = GetNodeInsideLoop(list);
+        return null != node;
+    }
+
+    private static Node<T> GetNodeInsideLoop<T>(this MovingPointer<T> list)
+    {
+        var fast = list.Root;
+        var slow = list.Root;
+        while (null != fast.Next && null != fast.Next.Next)
+        {
+            fast = fast.Next.Next;
+
+            slow = slow.Next;
+            if (fast == slow) return slow;
+        }
+        return null;
+    }
+}
