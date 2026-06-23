301a2a43cb292b7151da759c63ba4464328d63e1
kanwal019 <kanwal019@gmail.com>
2026-06-23 22:56:56 +0530

    ✨Add Queue class with enqueue, dequeue, and peek methods

---
+++ DataStructures.Core/Queues/Queue.cs
@@ -0,0 +1,63 @@
+using System;
+
+namespace DataStructures.Core.Queues;
+
+public class Node<T>
+{
+    public Node<T> Next { get; set; }
+    public T Value { get; set; }
+}
+
+public class Queue<T>
+{
+    protected Node<T> Head { get; set; }
+    protected Node<T> Tail { get; set; }
+
+    public void Enqueue(T value)
+    {
+        var valueNode = new Node<T> { Value = value, Next = null };
+        if (Tail is null)
+        {
+            Head = valueNode;
+            Tail = Head;
+            return;
+        }
+
+        Tail.Next = valueNode;
+        Tail = valueNode;
+    }
+
+    public T Dequeue()
+    {
+        if (Head is null)
+            throw new InvalidOperationException("The queue is empty!");
+
+        var result = Head.Value;
+        Head = Head.Next;
+        return result;
+    }
+
+    public T Peek()
+    {
+        if (Head is null)
+            throw new InvalidOperationException("The queue is empty!");
+
+        return Head.Value;
+    }
+
+    public override string ToString()
+    {
+        string result = "[";
+        var node = Head;
+        while (node is not null)
+        {
+            result += node.Value;
+            node = node.Next;
+
+            if (node is not null) 
+                result += ",";
+        }
+        result += "]";
+        return result;
+    }
+}
